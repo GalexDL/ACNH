e75e3fa15e5088f9ce9904ea4a4755d6592373a0
Galex <43304388+GalexDL@users.noreply.github.com>
2024-04-23 16:48:35 -0400

    Fixed Download

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+//Input to Paragraph
 function OutputText() {
     const inputText = document.getElementById("input").value;
     const output = document.getElementById("output");
-    output.innerHTML = inputText; // Set innerHTML to allow styling
+    output.innerHTML = inputText;
 }
-
-// Update output on page load and whenever the input changes
+// Update Output
 OutputText();
 document.getElementById("input").addEventListener("keyup", OutputText);
+
+///////////////////////////////////////////////////////////////////////
+
+//Input to Name
+function OutputNText() {
+    const inputText = document.getElementById("inputN").value;
+    const output = document.getElementById("outputN");
+    output.innerHTML = inputText;
+}
+// Update Output
+OutputNText();
+document.getElementById("input").addEventListener("keyup", OutputNText);
